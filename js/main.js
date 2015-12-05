097be3ae333f8abc3bad398527e51a68fe554cc2
Sota NAKAGAMI <athene609.cyl@gmail.com>
2015-12-05 17:38:17 +0900

    First.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function doitu() {
+  var words= ["ド","イ","ツ","の","ト","リ"],
+    wordsStr= words.toString(),
+    doitunotori= _.range(words.length),
+    i= 0;
+
+  do
+  {
+    var bakatare= _.sample(words);
+    $("pre").append(bakatare);
+    i=(i+1)|0;
+
+    var temp= _.rest(doitunotori);
+    temp.push(bakatare);
+    doitunotori= temp;
+
+  }while(doitunotori.toString()!==wordsStr);
+
+  $("pre").append("\n\n");
+
+  var totsuzennnodoitu= ["＿人人人人人人人人＿\n","＞　ドイツのトリ　＜\n","￣Y^Y^Y^Y^Y^Y^Y￣\n"];
+  jQuery.each(totsuzennnodoitu, function(){
+    $("pre").append(this);
+  });
+
+  $("pre").append("\nあなたは"+i+"羽目にドイツのトリを発見しました。");
+
+};
