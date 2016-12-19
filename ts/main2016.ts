c9455e788953dc88bacc0bef4ac20e59213d95c0
Sota.Nakagami <athene609.cyl@gmail.com>
2016-12-19 23:39:29 +0900

    js.Arrayの初期化わからんくなった

--- ts/main2016.ts
+++ ts/main2016.ts
@@ -1,3 +1,4 @@
+declare var _ :any;
 'use strict';
 class Tile {
 	private _id :number
@@ -7,43 +8,42 @@ class Tile {
 		this.imgUrl = this.setImgUrl(this._id)
 	}
 	private setImgUrl(id:number):string{
-		const urlPrefix = 'imgs/index2016/'
-		const urlSuffix = '.gif'
-		return urlPrefix + id.toString(10) + urlSuffix
+		return `imgs/index2016/${id.toString(10)}.gif`
 	}
 	get id(){
 		return this._id
 	}
 	get image(){
-		return `<img src="${this.imgUrl}" class="tile">`
+		return `<img src="${this.imgUrl}" class="tile" onClick="game.moveTile(${this.id})">`
 	}
 }
 
 class Game {
-	private positions :number[][]
-	private rightPos :number[][]
-	private time :number
-	private tiles :any[][]
-	private board = 'GameBoard'
+	private positions :number[][] = new Array(4)
+	private rightPos :number[][] = new Array(4)
+	private time = 0
+	private tiles :Tile[][]
+	static board = 'GameBoard'
 	constructor(){
 		this.setPositions()
 		this.setTiles()
 		this.render()
-		this.setEvents()
+		// this.start()
 	}
 	render(){
-		let d = this.gameBoard
+		// let d = this.gameBoard
 
 	}
 	private setPositions(){
-		let arr = [1,...,15]
+		let arr :number[] = _.shuffle(_.range(1,15))
 		let k = 1
 		for (let i=0; i<4; i++) {
+			this.positions.push(new Array(4))
+			this.rightPos.push(new Array(4))
 			for (let j=0; j<4; j++) {
-				let id = _.sample(arr)
-				_.drop(arr,id)
-				this.positions[i][j] = id
-				this.rightPos[i][j] = k
+				if (k===16) break
+				this.positions[i].push(arr[k-1])
+				this.rightPos[i].push(k)
 				k++
 			}
 		}
@@ -59,6 +59,23 @@ class Game {
 		}
 	}
 	get gameBoard(){
-		return document.getElementById(this.board);
+		return document.getElementById(Game.board)
 	}
+	public timeCount(){
+		this.time++
+	}
+	// public moveTile(id :number){
+
+	// }
 }
+
+
+
+
+
+document.getElementById(Game.board)!.addEventListener('contextmenu', (e:Event)=>{
+	e.preventDefault()
+	return false
+}, false)
+
+let game = new Game()
